260b2603d987fae2a9d14d8352c714e94a8601d2
Pelle Jacob Hansen <pellejacobhansen@outlook.dk>
2017-08-18 11:55:36 +0200

    Changes

--- ClientSocketExersice/Program.cs
+++ ClientSocketExersice/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
 
 namespace ClientSocketExersice
 {
@@ -10,6 +14,30 @@ namespace ClientSocketExersice
     {
         static void Main(string[] args)
         {
+            TcpClient server = new TcpClient("localhost", 20001);
+            NetworkStream stream = server.GetStream();
+            StreamReader streamReader = new StreamReader(stream);
+            StreamWriter streamWriter = new StreamWriter(stream);
+            streamWriter.AutoFlush = true;
+
+            new Thread(read =>  Read(streamReader)).Start();
+            new Thread(write =>  Write(streamWriter)).Start();      
+            
+        }
+        static public void Read(StreamReader reader)
+        {
+            while (true)
+            {
+                Console.WriteLine(reader.ReadLine());
+            }            
+        }
+
+        static public void Write(StreamWriter writer)
+        {
+            while (true)
+            {
+                writer.WriteLine(Console.ReadLine());
+            }            
         }
     }
 }
